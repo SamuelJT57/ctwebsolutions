bcfe73052f51359f9056e771bf4f96c5fc1381b6
Sam Turner <82002974+SamuelJT57@users.noreply.github.com>
2026-07-03 21:01:21 -0600

    Mega commit

--- script.js
+++ script.js
@@ -72,12 +72,11 @@ toggleButton.addEventListener("click", () => {
 const hamburger = document.getElementById("hamburger");
 const navLinks = document.getElementById("nav-links");
 
-hamburger.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
+if (hamburger && navLinks) {
+    hamburger.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
 
-    if (navLinks.classList.contains("active")) {
-        hamburger.textContent = "✕";
-    } else {
-        hamburger.textContent = "☰";
-    }
-});
+        hamburger.textContent =
+            navLinks.classList.contains("active") ? "✕" : "☰";
+    });
+}
